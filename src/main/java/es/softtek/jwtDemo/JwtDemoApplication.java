4850d145af13d7d8e140a2457f3df267086f1bea
admin <everydarylearn4yourself@gmail.com>
2022-07-24 13:00:29 +0530

    changing port from 8086 to 8085

--- src/main/java/es/softtek/jwtDemo/JwtDemoApplication.java
+++ src/main/java/es/softtek/jwtDemo/JwtDemoApplication.java
@@ -20,7 +20,7 @@ public class JwtDemoApplication {
 	public static void main(String[] args) {
 
 		Map<String, Object> prop = new HashMap<String, Object>();
-		prop.put("server.port", "8086");
+		prop.put("server.port", "8085");
 		SpringApplication app = new SpringApplication(JwtDemoApplication.class);
 		app.setDefaultProperties(prop);
 		app.run(args);
